f46cbe07a80b13f8fa284e36ad7a07e6d69ddb58
Sho Umezawa <s.umezawa0907@gmail.com>
2020-07-17 23:15:50 +0900

    ページ追加

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Home() {
+    return (
+        <>
+            <h1>learners</h1>
+            <Link href="/about"><a>about</a></Link>
+        </>
+    )
+}
